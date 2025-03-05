d3de88117e9036ac80aef109f445e1ef407db578
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2025-03-04 21:39:22 -0600

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mend/", // Set this to match your repository name
 });
